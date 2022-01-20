3818a76164a305ac633c5ff3fa627df4508622cd
lecocococo <chemwang13@naver.com>
2022-01-21 01:19:31 +0900

    AOP 를 이용한 메서드 실행시간 측정

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceApp;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,10 @@ public class SpringConfig {
         return new MemberService(memberRepository);
     }
 
+//    @Bean
+//    public TimeTraceApp timeTraceApp(){
+//        return new TimeTraceApp();
+//    }
 //    @Bean
 //    public MemberRepository memberRepository(){
 //        return new JdbcTemplateMemberRepository(dataSource);
